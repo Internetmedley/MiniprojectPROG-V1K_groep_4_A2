a21324b4d48f84c09d4592df244fab70a86819bd
Jelle-Jetze <jellejetze.hoekstra@student.hu.nl>
2018-10-12 13:54:09 +0200

    punten telling en RNG

---
+++ JJProg.py
@@ -0,0 +1,34 @@
+superhelden = ('iron man')
+# while True:
+#     import random
+#     ID = print(random.randint(1009146, 1015035))
+#     # random.rantint is tot en met
+#     if len(superhelden) > 1:
+#         continue
+#     else:
+#         break
+
+
+def punten_telling():
+    punten = 25
+    while True:
+        guess = input('welke superheld?: ')
+        if superhelden == guess:
+            print('je hebt goed geraden! je hebt nog ' + str(punten) + ' punten over')
+            return False
+        if guess != superhelden:
+            print('je hebt het fout')
+            punten -= 1
+            print('je hebt: ' + str(punten))
+            while True:
+                hint = input('wil je een hint? j/n (kost 3 punten)')
+                if hint == ('j'):
+                    punten -= 3
+                    print('je hebt nu: ' + str(punten))
+                    break
+                elif hint == ('n'):
+                    break
+                else:
+                    print('voer j of n in!')
+
+punten_telling()
