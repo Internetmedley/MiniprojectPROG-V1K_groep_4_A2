4fe3851d43eb59d7884b5e6e096c4cdc8c692356
Jelle-Jetze <jellejetze.hoekstra@student.hu.nl>
2018-10-16 14:28:42 +0200

    verbetde versie punten telling

--- JJProg.py
+++ JJProg.py
@@ -1,6 +1,7 @@
-from PROG import APIcall
-superhelden = APIcall.apicallcharID("1009610")
+from MiniprojectPROG import APIcall
+superhelden = APIcall.charIDtest()
 'superhelden naam in lowercase'
+
 
 def randomID():
     import random
@@ -8,27 +9,42 @@ def randomID():
     return ID
 
 
-def punten_telling():
+def punten_telling(superhelden):
+    superheld = str(superhelden)
+    superheld=superheld.lower()
     punten = 25
+    GeenPuntenKeerEen=0
     while True:
-        guess = str(input('welke superheld?: '))
-        guess.lower()
-        if guess == superhelden:
-            print('je hebt goed geraden! je hebt nog ' + str(punten) + ' punten over')
+        guess = input('Raad de superheld: ')
+        if superhelden == guess:
+            print('Je hebt goed geraden! je hebt nog {} punten over!\nBedankt voor het spelen :D!'.format(str(punten)))
             return False
         if guess != superhelden:
-            print('je hebt het fout')
+            print('Je hebt het fout\nProbeer het opnieuw!')
             punten -= 1
-            print('je hebt: ' + str(punten))
-            while True:
-                hint = input('wil je een hint? j/n (kost 3 punten)')
-                if hint == ('j'):
-                    punten -= 3
-                    print('je hebt nu: ' + str(punten))
-                    break
-                elif hint == ('n'):
-                    break
-                else:
-                    print('voer j of n in!')
+            print("Je hebt: {} punten".format(str(punten)))
+            if punten>0:
+                if punten>3:
+                    while True:
+                        hint = input('Wil je een hint? ja/nee (kost 3 punten)?: ')
+                        LowerHint=hint.lower()
+                        if LowerHint == ('ja'):
+                            punten -= 3
+                            print("Je hebt: {} punten".format(str(punten)))
+                            print('hint!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1')
+                            break
+                        elif LowerHint == ('nee'):
+                            break
+                        else:
+                            print('voer ja of nee in!')
+                elif GeenPuntenKeerEen<1:
+                    print("Je hebt niet genoeg punten om een hint aan te vragen.")
+                    GeenPuntenKeerEen=1
+            else:
+                print("Helaas heb je verloren omdat je geen punten meer over hebt.\nBedankt voor het spelen.")
+                print("De superheld was {}.".format(superhelden))
+                break
+
+punten_telling(superhelden)
 
 punten_telling()
