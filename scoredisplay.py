3cce199fc8d9f3bac235a24b7f252ff202e0a5c3
Internetmedley <remy.debruijn@student.hu.nl>
2018-10-17 20:50:30 +0200

    hi-scores is nu volledig functioneel, het werkt als er 10 dingen in de json staan

--- scoredisplay.py
+++ scoredisplay.py
@@ -1,23 +1,56 @@
 import json
 
-invoerNaam =  input("Name: ")
-score = 20
+# invoerNaam =  input("Name: ")
+# score = 20
 
-def high_score(name, score):
-    a_dict = {name: score}
-
-    with open('hi-scores.json') as f:
+def high_score_add(score):
+    "checkt of de high-score hoog genoeg is en dan voegt ie hem toe"
+    with open('hi-scores.json', 'r') as f:
         data = json.load(f)
 
-    data.update(a_dict)
+    if score > min(data.values()):
+        while True:
+            userName = input("Kies een gebruikersnaam(max. 14 characters): ")
+            if len(userName) > 14:
+                print("Uw gebruikersnaam is te lang. Probeer het nog eens.")
+                continue
+            elif userName in data.keys():
+                print("Uw gebruikersnaam is al in gebruik. Kies een andere.")
+                continue
+            else:
+                data.update({userName : score})
+                break
+    else:
+        print("Uw score is niet hoog genoeg om in de rankings te komen.")
+
+
+    lijstKeys = (sorted(data, key=data.__getitem__, reverse=True))
+    lijstValues = (sorted(data.values(), reverse=True))
+
+    data.clear()
+    for i in range(0, 10):
+        try:
+            data.update({lijstKeys[i]: lijstValues[i]})
+        except:
+            break
 
     with open('hi-scores.json', 'w') as f:
-        json.dump(data, f)
-
-    for key, value in data.items():
-            print("{:14}\t\t{:2}".format(key, value))
+            json.dump(data, f)
     return
 
 
-high_score(invoerNaam,score)
+def high_score_print():
+    with open('hi-scores.json', 'r') as f:
+        data = json.load(f)
+
+    hiscore = ''
+    for key, value in data.items():
+        hiscore += ("{:14}\t\t{:2}\n".format(key, value))
+
+    return print(hiscore)
+
+
+
+high_score_add(10)
+high_score_print()
 #kan niet zelfde key als eentje die al bestaat
