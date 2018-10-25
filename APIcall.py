b8d34de7b2c25159ed77d6e6827fbb578a19c717
ramon <ramon.petri@student.hu.nl>
2018-10-25 14:16:32 +0200

    Bug fixes on more than one thing

--- APIcall.py
+++ APIcall.py
@@ -6,99 +6,121 @@ import random
 
 
 def ID_test():
-	"geen arugment!!!"
-	loops = 0
+    "geen arugment!!!"
+    loops = 0
 
-	while True:
-		loops += 1
-		print(loops)
-		infile = open('nonexistentcharIDs.txt', 'r+')
-		lines = infile.readlines()
-		infile.close()
+    while True:
+        loops += 1
+        print(loops)
+        infile = open('nonexistentcharIDs.txt', 'r+')
+        lines = infile.readlines()
+        infile.close()
 
-		charID = random.randint(1009146, 1015035)
-		if str(charID) + '\n' in lines or str(charID) in lines:
-			# print("GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY")
-			continue
-		# print(charID)
+        charID = random.randint(1009146, 1015035)
+        if str(charID) + '\n' in lines or str(charID) in lines:
+            # print("GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY")
+            continue
+        # print(charID)
 
+        timestamp = str(time.time())
+        private_key = "c00f2975204127a291d19b4fc3d5b4978f18356f"  # niet veranderen
+        public_key = "ff2fe2f01fbfdc7d228605f74e3ad9fa"  # niet veranderen
 
-		timestamp = str(time.time())
-		private_key = "c00f2975204127a291d19b4fc3d5b4978f18356f"  # niet veranderen
-		public_key = "ff2fe2f01fbfdc7d228605f74e3ad9fa"  # niet veranderen
+        hash = hashlib.md5((timestamp + private_key + public_key).encode('utf-8'))
+        md5digest = str(hash.hexdigest())
 
-		hash = hashlib.md5((timestamp + private_key + public_key).encode('utf-8'))
-		md5digest = str(hash.hexdigest())
+        url = "https://gateway.marvel.com:443/v1/public/characters/{}".format(charID)
+        connection_url = url + "?ts=" + timestamp + "&apikey=" + public_key + "&hash=" + md5digest
+        # print(connection_url)
 
-		url = "https://gateway.marvel.com:443/v1/public/characters/{}".format(charID)
-		connection_url = url + "?ts=" + timestamp + "&apikey=" + public_key + "&hash=" + md5digest
-		#print(connection_url)
+        response = requests.get(connection_url)
+        jsontext = json.loads(response.text)
 
-		response = requests.get(connection_url)
-		jsontext = json.loads(response.text)
+        # om de JSON leesbaar te printen...
+        # print(json.dumps(jsontext, sort_keys=True, indent=4))
 
-		# om de JSON leesbaar te printen...
-		# print(json.dumps(jsontext, sort_keys=True, indent=4))
+        try:
+            jsontext['data']['results'][0]['description']
 
-		try:
-			jsontext['data']['results'][0]['description']
-			
-			if jsontext['data']['results'][0]['description'] == "":
-				infile = open('nonexistentcharIDs.txt', 'a')
-				infile.write(str(charID) + '\n')
-				infile.close()
-				continue
-			else:
-				break
-		except KeyError:
-			infile = open('nonexistentcharIDs.txt', 'a')
-			infile.write(str(charID) + '\n')
-			infile.close()
-			continue
-			
-	with open('informatie.json', 'w') as f:
-		json.dump(jsontext, f)
-		
-	return jsontext
+            if jsontext['data']['results'][0]['description'] == "":
+                infile = open('nonexistentcharIDs.txt', 'a')
+                infile.write(str(charID) + '\n')
+                infile.close()
+                continue
+            else:
+                break
+        except KeyError:
+            infile = open('nonexistentcharIDs.txt', 'a')
+            infile.write(str(charID) + '\n')
+            infile.close()
+            continue
+
+    with open('informatie.json', 'w') as f:
+        json.dump(jsontext, f)
+    return
 
 
-def hero_name(info):
-	"variabele informatie als argument"
-	naam = info['data']['results'][0]['name']
-	if '(' in naam:
-		return naam.replace(naam[naam.index('(') -1:], '')
-	else:
-		return naam
+def hero_name():
+    "variabele informatie als argument"
+    with open('informatie.json', 'r') as f:
+        info = json.load(f)
+        naam = info['data']['results'][0]['name']
+        if '(' in naam:
+            return naam.replace(naam[naam.index('(') - 1:], '')
+        else:
+            return naam
 
-def	hero_letters(info):
-	"returnt de hint hoeveel letters de naam bevat"
-	naam = info['data']['results'][0]['name']
-	if '(' in naam:
-		naam = naam.replace(naam[naam.index('(') -1:], '')
-		hint = 'De naam bevat {} letters.'.format(len(naam))
-		return hint
-	else:
-		hint = 'De naam bevat {} letters.'.format(len(naam	))
-		return hint
 
-def hero_description(info):
-	"variabele informatie als argument en haalt naam uit de description"
-	naam = info['data']['results'][0]['name']
-	if '(' in naam:
-		naam = naam.replace(naam[naam.index('(') - 1:], '')
-	hint = info['data']['results'][0]['description'].replace(naam, '*****')
-	return hint
+def hero_letters():
+    "returnt de hint hoeveel letters de naam bevat"
+    with open('informatie.json', 'r') as f:
+        info = json.load(f)
+        naam = info['data']['results'][0]['name']
+        if '(' in naam:
+            naam = naam.replace(naam[naam.index('(') - 1:], '')
+            hint = 'De naam bevat {} letters.'.format(len(naam))
+            return hint
+        else:
+            hint = 'De naam bevat {} letters.'.format(len(naam))
+            return hint
 
-def	eerste_letter(info):
-	":returns de eerste letter van naam"
-	naam = info['data']['results'][0]['name']
-	return naam[0]
 
-def hero_comics(info):
-	":returns de eerste letter van naam"
-	naam = info['data']['results'][0]['name']
-	return naam[0]
+def hero_description():
+    "variabele informatie als argument en haalt naam uit de description"
+    with open('informatie.json', 'r') as f:
+        info = json.load(f)
+        naam = info['data']['results'][0]['name']
+        if '(' in naam:
+            naam = naam.replace(naam[naam.index('(') - 1:], '')
+        hint = info['data']['results'][0]['description'].replace(naam, '*****')
+        return hint
 
+
+def eerste_letter():
+    ":returns de eerste letter van naam"
+    with open('informatie.json', 'r') as f:
+        info = json.load(f)
+        naam = info['data']['results'][0]['name']
+        return naam[0]
+
+
+def hero_comics():
+    ":returns de eerste letter van naam"
+
+    comics = ''
+    with open('informatie.json', 'r') as f:
+        info = json.load(f)
+        for item in info['data']['results'][0]['comics']['items']:
+            comics += "{} : ".format(item['name'])
+        return comics
+
+
+def hero_image_URL():
+    with open('informatie.json', 'r') as f:
+        info = json.load(f)
+        imgURL = info['data']['results'][0]['thumbnail']['path'] + '.' + info['data']['results'][0]['thumbnail'][
+            'extension']
+        return imgURL
 
 # informatie = ID_test()
 # print(hero_description(informatie))
@@ -106,9 +128,4 @@ def hero_comics(info):
 # print(eerste_letter(informatie))
 
 
-
 # print(json.dumps(informatie, sort_keys=True, indent=4))
-
-
-
-
--- GUI.py
+++ GUI.py
@@ -1,21 +1,66 @@
 from tkinter import *
-from tkinter.messagebox import showinfo
+import APIcall
 
+score = 25
+hintKeuzeDict = {'1' : APIcall.hero_description(),
+                 '2' : APIcall.hero_letters(),
+                 '3' : APIcall.eerste_letter(),
+                 '4' : APIcall.hero_comics()}
+
+
+
+def hintButton1():
+    global score
+    labelGuessAwnser["text"] = hintKeuzeDict["1"]
+    hint1Button.destroy()
+    score -= 3
+    scoreLabel["text"] = "SCORE: {}".format(score)
+
+
+def hintButton2():
+    global score
+    labelGuessAwnser["text"] = hintKeuzeDict["2"]
+    hint2Button.destroy()
+    score -= 3
+    scoreLabel["text"] = "SCORE: {}".format(score)
+
+def hintButton3():
+    global score
+    labelGuessAwnser["text"] = hintKeuzeDict["3"]
+    hint3Button.destroy()
+    score -= 3
+    scoreLabel["text"] = "SCORE: {}".format(score)
+
+
+def hintButton4():
+    global score
+    labelGuessAwnser["text"] = hintKeuzeDict["4"]
+    hint4Button.destroy()
+    score -= 3
+    scoreLabel["text"] = "SCORE: {}".format(score)
 
 def guessButtonClicked():
-    labelGuessAwnser["text"] = enterSuperHero.get()
-    print("Check tegen API CALL LINE 88")
+    global score
+    if enterSuperHero.get().lower() == APIcall.hero_name().lower():
+        labelGuessAwnser["text"] = "U heeft het goed geraden!"
+    else:
+        labelGuessAwnser["text"] = "U heeft het fout geraden!"
+        score -= 1
+        scoreLabel["text"] = "SCORE: {}".format(score)
+
 
 def buildStartScreen():
     howToPlayScreen.pack_forget()
     highScoreScreen.pack_forget()
     mainGame.pack_forget()
+    AboutPage.pack_forget()
     startScreen.pack(fill=BOTH, expand=True)
     quitButton.pack(side=BOTTOM)
     playButton.pack(side=BOTTOM)
 
 
 def highScores():
+    AboutPage.pack_forget()
     howToPlayScreen.pack_forget()
     startScreen.pack_forget()
     mainGame.pack_forget()
@@ -23,40 +68,54 @@ def highScores():
 
 
 def howToPlay():
+    AboutPage.pack_forget()
     highScoreScreen.pack_forget()
     mainGame.pack_forget()
     startScreen.pack_forget()
     howToPlayScreen.pack(fill=BOTH, expand=True)
 
+
 def mainGameWindow():
+    emptyMenu = Menu(root)
+    root.config(menu=emptyMenu)
+    AboutPage.pack_forget()
     howToPlayScreen.pack_forget()
     highScoreScreen.pack_forget()
     startScreen.pack_forget()
     mainGame.pack(fill=BOTH, expand=True)
 
+
+def AboutWindow():
+    howToPlayScreen.pack_forget()
+    highScoreScreen.pack_forget()
+    startScreen.pack_forget()
+    mainGame.pack_forget()
+    AboutPage.pack(fill=BOTH, expand=True)
+
 root = Tk()
 root.title("SuperHero The Game")
-root.geometry("600x600")
+root.geometry("1920x1080")
 
 
 #Build startscreen and atributes
 startScreen = Frame(master=root, bg="black")
 startScreen.pack(fill=BOTH, expand=True)
-playButton = Button(master=startScreen, text="PLAY", command=mainGameWindow, height=2, width=40, cursor="hand2")
+playButton = Button(master=startScreen, text="PLAY", command=lambda: [mainGameWindow(), APIcall.ID_test()], height=2, width=40, cursor="hand2")
 quitButton = Button(master=startScreen, text="QUIT", command=root.quit, height=2, width=40, cursor="hand2")
-quitButton.pack(side=BOTTOM, pady=10)
-playButton.pack(side=BOTTOM, pady=20)
+quitButton.pack(side=BOTTOM, pady=55)
+playButton.pack(side=BOTTOM, pady=2)
 
 #Build highscorescreen and atributes
 highScoreScreen = Frame(master=root, bg="black")
 highScoreScreen.pack(fill=BOTH, expand=True)
-backButtonScore = Button(master=highScoreScreen, text='Back', command=buildStartScreen)
-backButtonScore.pack(side=BOTTOM, pady=20)
+backButtonScore = Button(master=highScoreScreen, text='HOME', command=buildStartScreen)
+backButtonScore.pack(side=BOTTOM, padx=20, pady=60)
+
 
 #Build howtoplayscreen and atributes
 howToPlayScreen = Frame(master=root, bg="black")
 howToPlayScreen.pack(fill=BOTH, expand=True)
-backButtonHowTo = Button(master=howToPlayScreen, text='Back', command=buildStartScreen)
+backButtonHowTo = Button(master=howToPlayScreen, text='HOME', command=buildStartScreen)
 text = Text(howToPlayScreen, bg="black", fg="white")
 text.insert(INSERT,  "When you start to play the game you get 25 points. \n"
                     "You have 2 choices: You can either buy a hint or you can guess the character. \n"
@@ -69,13 +128,14 @@ text.insert(INSERT,  "When you start to play the game you get 25 points. \n"
                     "tip 4 : You get to see how many characters there are in the name of the character. \n"
                     "If you guess the character wrong, you loose 1 point. \n")
 text.pack()
-backButtonHowTo.pack(side=BOTTOM, pady=20)
+backButtonHowTo.pack(padx=100, pady=20)
 
 hintDisplayDict = {'1': "Press 1 description of the character.",
                    '2': "Press 2 for the number of characters in the name.",
                    '3': "de eerste letter van het te raden character.",
                    '4': "een aantal comics waar het te raden character in voorkomt."}
 
+#Build the main game window and build atributes
 mainGame = Frame(master=root, bg="black")
 mainGame.pack(fill=BOTH, expand=True)
 enterSuperHero = Entry(master=mainGame)
@@ -86,22 +146,82 @@ labelHints = Label(master=mainGame, bg="black", fg="white", text="{}\n {}\n {}\n
 labelHints.pack(side=TOP, pady=10, padx=0)
 labelGuessAwnser = Label(master=mainGame, text="", fg="white", bg="black")
 guessButton = Button(master=mainGame, text="GUESS", command=guessButtonClicked)
+giveUpButton = Button()
 guessButton.pack()
 labelGuessAwnser.pack()
+scoreLabel = Label(master=mainGame, bg="black", fg="white", text="SCORE: 25")
+scoreLabel.place(relx=1.0, rely=0.0, anchor=NE)
+hint1Button = Button(master=mainGame, text="Give description", command=hintButton1)
+hint2Button = Button(master=mainGame, text="Give amount letters", command=hintButton2)
+hint3Button = Button(master=mainGame, text="Give first letter of name", command=hintButton3)
+hint4Button = Button(master=mainGame, text="Give comics in which the character appears", command=hintButton4)
+hint1Button.place(relx=0.45, rely=0.3)
+hint2Button.place(relx=0.53, rely=0.3)
+hint3Button.place(relx=0.63, rely=0.3)
+hint4Button.place(relx=0.26, rely=0.3)
+
+
+#Build the about page and its atributes
+AboutPage = Frame(master=root, bg="black")
+labelframe1 = Label(master=AboutPage,
+              text='About us and the game: \n',
+              font='times 16 bold',
+              fg= 'white',
+              bg='black',
+              width= 10000,
+              height=5)
+labelframe1.pack(side=TOP)
+
+labelframe1= Label(master=AboutPage,
+              text='Our opinions going into this project: \n'
+                   '\n'
+                   'This game was made by a copple of students, studying at the HU in Utrecht.\n'
+                   'We are five boys around the ages of 20 hoping to become an experienced programmer\n'
+                   '"Tobias S: I realy enjoyed making this game even thought sometimes the error messages got the better of me."\n'
+                   '"Remy d B: When we started, I did not know if it would be fun but after working on it with my team I started to enjoy this project a lot more."\n'
+                   '"Jesse B: Starting the project was quite a struggle because I fas far behind with my knowledge but I have caught up for and thanks to my team, so I wanted to thank them again."\n'
+                   '"Ramon P: I had difficulty visualising the project but after all the hard work we put I had a lot of fun."\n'
+                   '"Jelle-Jetze H: I had difficulty taking the lead as projectleader but after all backup I received from my team I got a lot more confident in my role."\n',
+              fg= 'white',
+              bg='black',
+              font='times 11',
+              width= 100000,
+              height=10)
+labelframe1.pack(side=TOP)
+
+labelframe1= Label(master=AboutPage,
+              text='Reason for creating this game:\n'
+                   '\n'
+                   'This game was originally a school project. We could choose from many different assignments but this one just stood out as project.\n'
+                   'But what started as school-project ended in a fun hobby project with some new friends.\n'
+                   "We let our imagination go loose on this project and we realy did not try to hold back",
+              fg= 'white',
+              bg='black',
+              font='times 11',
+              width= 1000000,
+              height=7)
+labelframe1.pack(side=LEFT)
+
+frame= Frame(master=root, bg='black')
+frame.pack()
+
+downframe= Frame(master=root)
+downframe.pack(side=BOTTOM)
+backButtonAbout = Button(master=AboutPage, text="HOME", command=buildStartScreen)
 
 root.iconbitmap("C:/Users/ramon/Downloads/marvel.ico")
 buildStartScreen()
 
-# Drop down menu op begin scherm
+# Add a Drop-down menu to the startscreen
 menubar = Menu(root)
 
 helpmenu = Menu(menubar, tearoff=0)
 helpmenu.add_command(label="How to play", command=howToPlay)
-helpmenu.add_command(label="About us")
+helpmenu.add_command(label="About us", command=AboutWindow)
 helpmenu.add_command(label="Highscores", command=highScores)
 menubar.add_cascade(label="Menu", menu=helpmenu)
-
 root.config(menu=menubar)
 
+# start the application
 root.mainloop()
 
